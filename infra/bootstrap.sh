aa70ed298e8010db2681f06381e83990b63d8baf
Luiz Alfredo Thomasini <52940168+luizth@users.noreply.github.com>
2026-08-17 22:29:37 -0300

    Update bootstrap.sh to modify PATH in .bashrc
    Add PATH export to .bashrc and source it.

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -11,6 +11,8 @@ sudo apt install -y \
     htop \
     ca-certificates
 
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc && source ~/.bashrc
+
 "$INFRA_DIR/scripts/install-python.sh"
 
 "$INFRA_DIR/scripts/install-claude.sh"
